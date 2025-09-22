ffc879b1780198ad076f0f7a426bbe968c8be62d
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2025-09-22 19:11:51 +0530

    Adding middlewares

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -10,9 +10,8 @@ export async function GET(request:NextRequest){
       const userId=await getDataFromToken(request);
       const user = await User.findOne({ _id: userId }).select("-password");
       return NextResponse.json({message:"User found",data:user});
-    } catch (error: any) {
-        return NextResponse.json({error:error.message},{status:400});
-        
+    } 
+    catch (error: any) {
+      return NextResponse.json({error:error.message},{status:400});
     }
-	
 }
